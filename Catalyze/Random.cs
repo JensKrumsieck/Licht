b77cbb348fc28f3db696af24edc1dcbb81423df7
Jens Krumsieck <jenso666@live.de>
2023-09-06 14:43:49 +0200

    add more random methods

--- Catalyze/Random.cs
+++ Catalyze/Random.cs
@@ -19,16 +19,31 @@ public static class Random
         seed = PcgHash(ref seed);
         return seed / (float) uint.MaxValue;
     }
+    
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static float Float(ref uint seed, int min, int max)
+    {
+        var f = Float(ref seed);
+        return f * (max - min) + min;
+    }
+    
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3 Vec3(ref uint seed, float min, float max) =>
         new(Float(ref seed) * (max - min) + min, 
             Float(ref seed) * (max - min) + min,
             Float(ref seed) * (max - min) + min);
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 Vec3(ref uint seed) =>
+        new(Float(ref seed),
+            Float(ref seed),
+            Float(ref seed));
     
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static Vector3 InUnitSphere(ref uint seed) => Vector3.Normalize(Vec3(ref seed, -1, 1));
 
-    public static Vector3 InHemisphere(ref uint seed, Vector3 normal)
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static Vector3 OnHemisphere(ref uint seed, Vector3 normal)
     {
         var inUnitSphere = InUnitSphere(ref seed);
         return Vector3.Dot(inUnitSphere, normal) > 0 ? inUnitSphere : -inUnitSphere;
