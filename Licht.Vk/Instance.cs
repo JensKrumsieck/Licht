b5fdd43aa8699cd023e373fb58e40617446829ff
Jens Krumsieck <jenso666@live.de>
2023-10-11 22:47:51 +0200

    del

--- Licht.Vk/Instance.cs
+++
@@ -1,9 +0,0 @@
-﻿global using static Licht.Vulkan.Instance;
-using Silk.NET.Vulkan;
-
-namespace Licht.Vulkan;
-
-public partial class Instance
-{
-    public static readonly Vk vk = Vk.GetApi();
-}
