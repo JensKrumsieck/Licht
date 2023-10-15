9f68ba34e9736f94f9fe5e9998edad9a9aa13651
Jens Krumsieck <jenso666@live.de>
2023-10-16 00:01:04 +0200

    sign off

---
+++ Licht/Rendering/Vertex.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Licht.Rendering;
+
+public struct Vertex
+{
+    public Vector3 Position;
+    public Vector3 Color;
+    public Vector3 Normal;
+    public Vector2 TextureCoordinates;
+}
---
+++ Licht/Scene/TransformComponent.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+
+namespace Licht.Scene;
+
+public struct TransformComponent 
+{
+    public Vector3 Translation = Vector3.Zero;
+    public Vector3 Scale = Vector3.One;
+    public Vector3 Rotation = Vector3.Zero;
+    public TransformComponent() { }
+}
